7520dbd9f2d5f804b00c8866ffaecf1b90fe2248
Pat Maddox <patmaddox@me.com>
2010-08-24 18:43:02 -0700

    work for rspec 2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,28 +1,26 @@
 require 'rubygems'
-require 'spec'
+require 'rspec'
 
 Dir[File.dirname(__FILE__) + '/../lib/**/*.rb'].each do |ruby_file|
   require ruby_file
 end
 
-class OneAtATimeReporter < Spec::Runner::Reporter
+class OneAtATimeReporter < RSpec::Core::Reporter
   def example_started(example)
     super unless @has_failed
   end
 
-  def example_failed(example, error)
+  def example_failed(example)
     unless @has_failed
       @has_failed = true
       super
     end
   end
-
-  def example_finished(example, error=nil)
-    super unless @has_failed
-  end
 end
 
-Spec::Runner.options.reporter = OneAtATimeReporter.new(Spec::Runner.options)
-
-
+class RSpec::Core::Configuration
+  def reporter
+    @reporter ||= OneAtATimeReporter.new(formatter)
+  end
+end
 
